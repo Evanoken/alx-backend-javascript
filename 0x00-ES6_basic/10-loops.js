b2e3597a716f370577da587b590e8c155a821268
Evans Ngugi <ngugie649@gmail.com>
2023-07-04 12:06:14 -0400

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const result = [];
-
-  for (let value of array) {
-    result.push(appendString + value);
+  const returnArray = [];
+  for (const element of array) {
+    returnArray.push(appendString + element);
   }
 
-  return result;
+  return returnArray;
 }
-
