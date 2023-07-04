0df7ee1eeac95217855e244efdc10787a37e7ddc
Evans Ngugi <ngugie649@gmail.com>
2023-07-04 10:41:34 -0400

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-let task = false;
-let task2 = true;
+  let task = false;
+  let task2 = true;
 
-if (trueOrFalse) {
-task = true;
-task2 = false;
-}
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+    // These variables are now block-scoped and won't affect the outer variables
+  }
 
-return [task, task2];
+  return [task, task2];
 }
